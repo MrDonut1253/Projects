06c4c01f485b0cecbcfe49ae1242a301adb4366d
MrDonut1253 <106107551+MrDonut1253@users.noreply.github.com>
2022-08-18 22:51:08 +0200

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -24,37 +24,45 @@ def divider(number1,number2):
 
 while True:
     print("--Calculator--")
+    print("")
 
     while True:
         try:
-            number1 = int(input("enter the first number: "))       
+            number1 = float(input("enter the first number: "))       
         except ValueError:
             print("your input is not a number")
             continue
         else:
             break
+    
 
     while True:
         try:
-            number2 = int(input("enter the second number: "))       
+            number2 = float(input("enter the second number: "))       
         except ValueError:
             print("your input is not a number")
             continue
         else:
             break
-
+    print("")
     choice = ""
     while choice not in CHOICES:
         choice = input("What do you want to do? (add/subtract/multiply/divide) : ")
         if choice == "add" or choice =="+":
+            print("")
             print(adder(number1,number2))
         elif choice =="substract" or choice =="-":
+            print("")
             print(substractor(number1,number2))
         elif choice =="multiply" or choice =="*":
+            print("")
             print(multiplier(number1, number2))
         elif choice =="divide" or choice == ":":
+            print("")
             print(divider(number1,number2))
-
+    
+    print("")
+    
     again=""
     while again not in AGAIN_LIST:
         again = input("Calculate again? (Yes/No) : ")
@@ -62,7 +70,7 @@ while True:
         break
          
 print("Bye!")
-time.sleep(1)
+time.sleep(0.5)
 
 
   
